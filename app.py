b9431c1014f552cb9fd99e9340b34299a6881696
tiainensa <144015802+tiainensa@users.noreply.github.com>
2025-04-05 14:45:25 +0300

    change port

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def get_data():
     return {"message": "Data received", "data": data}, 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
